ccd1147feb7649a100450c4a0b212e3e42af6ae8
Gilberto Vasquez Mera <drago19.07.95@gmail.com>
2022-12-08 17:34:54 -0600

    Initial Commit

---
+++ Clima/ClimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimaApp.swift
+//  Clima
+//
+//  Created by Gilberto Mera on 08/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ClimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
